827ca2fe39e282f11ae63157f1a3d5e3ed3ec756
Mohamed Ridjali <md.ridjali@gmail.com>
2023-10-07 15:12:45 +0200

    [CHANGES] Composable states

--- app/src/main/java/com/phoenix/samples/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/phoenix/samples/basicscodelab/MainActivity.kt
@@ -14,7 +14,10 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.phoenix.samples.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -44,7 +47,9 @@ fun MyApp(names: List<String> = listOf("World", "Compose")) {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
@@ -53,12 +58,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             Column(
                 modifier = Modifier
                     .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
             OutlinedButton(onClick = { }) {
-                Text("Show more")
+                Text(
+                    text = if(expanded.value) "Show less" else "Show more",
+                    color = Color.White,
+                )
             }
         }
     }
