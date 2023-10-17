5444ea2d888b00160c1ae1b8263dc3a865de0ee9
Mohamed Ridjali <md.ridjali@gmail.com>
2023-10-17 21:27:20 +0200

    minr

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,12 @@
 plugins {
     id("com.android.application") version "8.2.0-alpha03" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.sonarqube") version "4.2.1.3168"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "xmethos_BasicsCodelab_AYs_D4E-STckWwAXsmm1")
+        property("sonar.projectName", "BasicsCodelab")
+    }
 }
